c6a0b6f69cb0b8135cbd9f6d80fd80a7a6b6f66a
kashif khan <kashifk@folio3.com>
2025-01-17 14:18:57 +0500

    added operator mapping

--- filters.go
+++ filters.go
@@ -1,5 +1,7 @@
 package mongoq
 
+import "go.mongodb.org/mongo-driver/bson"
+
 // Operator represents filter operations.
 type Operator int
 
@@ -19,11 +21,35 @@ type Filter struct {
 	Op    Operator
 }
 
+// newFilter creates a new filter with validation and operator mapping.
 func newFilter(key string, value interface{}, op ...Operator) Filter {
-	f := Filter{Key: key, Value: value, Op: Equal}
+	if key == "" {
+		panic("key cannot be empty")
+	}
 
+	if value == nil {
+		panic("value cannot be nil")
+	}
+
+	f := Filter{Key: key, Value: value, Op: Equal}
 	if len(op) > 0 {
 		f.Op = op[0]
+	}
+
+	// Map operators to MongoDB query syntax
+	switch f.Op {
+	case Equal:
+		f.Value = value
+	case NotEqual:
+		f.Value = bson.M{"$ne": value}
+	case GTE:
+		f.Value = bson.M{"$gte": value}
+	case LTE:
+		f.Value = bson.M{"$lte": value}
+	case Like:
+		f.Value = bson.M{"$regex": value}
+	case IgnoreCase:
+		f.Value = bson.M{"$regex": value, "$options": "i"}
 	}
 
 	return f
